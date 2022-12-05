4773771dd99b1159e0a6f83a3182f8d9e8fe1822
KasiwaMochiD <118461962+KasiwaMochiD@users.noreply.github.com>
2022-12-05 17:00:54 +0900

    add mosaic process

--- detect_face.py
+++ detect_face.py
@@ -1,3 +1,4 @@
+import sys
 import cv2
 
 class DetectFace() :
@@ -12,33 +13,48 @@ class DetectFace() :
 
     cascade_path = "./resource/haarcascade_frontalface_default.xml"
     face_cascade = cv2.CascadeClassifier(cascade_path)
-    def __init__(self) :
-        self.image_file = ""
-        
+    
+    #pre process
+    def __init__(self, image_file) :
+        self.image_file = image_file
+        self.mosaic_ratio = 0.2
+        self.post_process_mode = "mosaic"
 
-    def write_rectangle(self, detect_faces, image) :
-        for x, y, w, h in detect_faces :
-            cv2.rectangle(image, (x, y), (x+w, y+h), (255, 255, 255), 2)
-        return image
-
+        self.detected_faces =  list()
+        self.image = cv2.imread(self.image_file)
+        if self.image is None :
+            print("Image Load Error")
+            sys.exit(1)
 
     def detect_face(self) :
-        image = cv2.imread(self.image_file)
-        image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image_gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        self.detected_faces = self.face_cascade.detectMultiScale(image_gray)
 
-        detect_faces = self.face_cascade.detectMultiScale(image_gray)
+    def write_rectangle(self) :
+        for x, y, w, h in self.detected_faces :
+            cv2.rectangle(self.image, (x, y), (x+w, y+h), (255, 255, 255), 2)
+    
+    def mosaic_face(self) :
+        for x, y, w, h in self.detected_faces :
+            small_image = cv2.resize(self.image[y:y+h, x:x+w], None, fx = self.mosaic_ratio, fy = self.mosaic_ratio, interpolation = cv2.INTER_NEAREST)
+            self.image[y:y+h, x:x+w] = cv2.resize(small_image, (w,h), interpolation = cv2.INTER_NEAREST) 
 
-        #矩形描画テスト
-        rect_image = self.write_rectangle(detect_faces, image)
-
-        cv2.imshow("test_result", rect_image)
+    def post_process(self) :
+        if self.post_process_mode == "rectangle" :
+            self.write_rectangle()
+        elif self.post_process_mode == "mosaic" :
+            self.mosaic_face()
+        else :
+            print("Post Process Mode Error")
+            sys.exit(1)
+        
+        cv2.imshow(f"test_result mode{self.post_process_mode}", self.image)
         cv2.waitKey(0)
 
-
 if __name__ == '__main__' :
-    detect_test = DetectFace()
-    detect_test.image_file = "./resource/test.jpg"
+    detect_test = DetectFace("./resource/test.jpg")
     detect_test.detect_face()
+    detect_test.post_process()
 
 
 
