802bb95f55aed100f7580e0cb264f92f2031f42d
KasiwaMochiD <118461962+KasiwaMochiD@users.noreply.github.com>
2022-12-05 13:51:46 +0900

    push face detect

--- detect_face.py
+++ detect_face.py
@@ -1,8 +1,42 @@
 import cv2
 
-class DetectFace :
+class DetectFace() :
     """Face detection for a image """
-    def __init__(self) :
-        self.image = ""
 
-    def detect_face(image) :
+    cascade_path = "./Mosaic_app/haarcascade_frontalface_default.xml"
+    face_cascade = cv2.CascadeClassifier(cascade_path)
+    def __init__(self) :
+        self.image_file = ""
+        
+
+    def write_rectangle(self, detect_faces, image) :
+        for x, y, w, h in detect_faces :
+            cv2.rectangle(image, (x, y), (x+w, y+h), (255, 255, 255), 2)
+        return image
+
+
+    def detect_face(self) :
+        image = cv2.imread(self.image_file)
+        image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        detect_faces = self.face_cascade.detectMultiScale(image_gray)
+
+        print(detect_faces)
+        #矩形描画テスト
+        rect_image = self.write_rectangle(detect_faces, image)
+
+        cv2.imshow("test_result", rect_image)
+        cv2.waitKey(0)
+
+
+if __name__ == '__main__' :
+    detect_test = DetectFace()
+    detect_test.image_file = "./Mosaic_app/many_face_test.jpg"
+    detect_test.detect_face()
+
+
+
+
+
+    
+
