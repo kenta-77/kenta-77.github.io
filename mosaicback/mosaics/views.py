61cd616475f63737069bd54cc228ffe06e469c2d
kenta-77 <pmd46b23@s.okayama-u.ac.jp>
2022-12-26 12:08:43 +0900

    uuidをidにする

--- mosaicback/mosaics/views.py
+++ mosaicback/mosaics/views.py
@@ -7,7 +7,6 @@ from .models import Mosaic
 from .serializers import MosaicSerializer
 import cv2
 from .process_image.detect_face import DetectFace
-import os
 
 @api_view(["GET","POST"]) #GETとPOSTメソッドを受け付ける
 def mosaic_upload(request):
@@ -19,41 +18,29 @@ def mosaic_upload(request):
     serializer = MosaicSerializer(data=request.data)
     if serializer.is_valid():
       serializer.save() #postデータをDBに登録
-      mosaic = Mosaic.objects.get(id=serializer.data["id"])
-      org_path = mosaic.image.url
-      gray_path = str(settings.BASE_DIR) + "/media/results/result.jpg"
-      print(gray_path)
-      #--画像処理--#
-      # cwd_path = os.getcwd()
-      # print(cwd_path)
-      # detect_test = DetectFace("./mosaics/process_image/resource/", "image_test2.jpeg")
-      detect_test = DetectFace(str(settings.BASE_DIR), org_path)
-      detect_test.detect_face()
-      detect_write = detect_test.write_rectangle()
-      detect_stamp = detect_test.stamp_smile_face()
-
-      mosaic.result = "results/result.jpg"
-      mosaic.save()
-      serializer = MosaicSerializer(mosaic)
+      #----モザイク化----#
+      mosaic = Mosaic.objects.get(id=serializer.data["id"]) #送信された画像のidを取得
+      org_path = mosaic.image.url #送信された画像のurlを取得
+      # gray_path = str(settings.BASE_DIR) + "/media/results/result.jpg"
+      detect_test = DetectFace(str(settings.BASE_DIR), org_path) #モザイククラスのインスタンス作成
+      detect_test.detect_face() #顔検知メソッドを実行
+      detect_write = detect_test.write_rectangle() #検知した顔の領域を表示するメソッドを実行
+      detect_stamp = detect_test.stamp_smile_face() #検知した顔にスタンプを表示するメソッドを実行
+      mosaic.result = "results/result.jpg" #結果画像のurlをDBに登録
+      mosaic.save() #変更内容を登録
+      serializer = MosaicSerializer(mosaic) #データをシリアライズ
       return Response(serializer.data, status.HTTP_201_CREATED)
     return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
 
-@api_view(["GET"])
-def mosaic_download(request): #uploadされたデータを処理して送信するメソッド
-  if request.method == "GET":
-    mosaic = Mosaic.objects.get(id=1)
-    org_path = mosaic.image.url
-    gray_path = str(settings.BASE_DIR) + "/media/results/result.jpg"
-    gray(org_path,gray_path)
-    mosaic.result = "results/result.jpg"
-    mosaic.save()
-    serializer = MosaicSerializer(mosaic)
-    return Response(serializer.data)
-
-def gray(img_path,output_path):
-  org_path = str(settings.BASE_DIR) + img_path
-  org_img = cv2.imread(org_path)
-  gray_img = cv2.cvtColor(org_img,cv2.COLOR_BGR2GRAY)
-  cv2.imwrite(output_path,gray_img)
+# @api_view(["GET"])
+# def mosaic_download(request): #uploadされたデータを処理して送信するメソッド
+#   if request.method == "GET":
+#     mosaic = Mosaic.objects.get(id=1)
+#     org_path = mosaic.image.url
+#     gray_path = str(settings.BASE_DIR) + "/media/results/result.jpg"
+#     mosaic.result = "results/result.jpg"
+#     mosaic.save()
+#     serializer = MosaicSerializer(mosaic)
+#     return Response(serializer.data)
 
 # Create your views here.
