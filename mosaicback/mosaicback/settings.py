7a3f0fb42d5b8533c861f21ca99f31b718050e6b
kenta-77 <pmd46b23@s.okayama-u.ac.jp>
2023-02-14 16:35:16 +0900

    gitignoreでキャッシュ削除

--- mosaicback/mosaicback/settings.py
+++ mosaicback/mosaicback/settings.py
@@ -82,9 +82,9 @@ WSGI_APPLICATION = "mosaicback.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        'NAME': 'detabasename',
-        'USER': 'postgres',
-        'PASSWORD': '123123',
+        'NAME': 'DetaBaseName',
+        'USER': 'mosaicer',
+        'PASSWORD': 'MoPos-88er,',
         'HOST': '127.0.0.1',
         'PORT': '5432',
             #example@example.com
