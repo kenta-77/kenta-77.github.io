033577974bdbbb8e7bd9b9fdfd53abb45bc3a8dd
kenta-77 <pmd46b23@s.okayama-u.ac.jp>
2023-03-20 11:27:51 +0900

    google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,41 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from "next/app";
 import theme from "../components/theme";
+import Script from "next/script";
+import * as gtag from "lib/gtag";
+import { useRouter } from "next/router";
+ import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+   useEffect(() => {
+     const handleRouterChange = (url: any) => {
+       gtag.pageview(url);
+     };
+     router.events.on("routeChangeComplete", handleRouterChange);
+     return () => {
+       router.events.off("routeChangeComplete", handleRouterChange);
+     };
+   }, [router.events]);
+
   return (
     <ChakraProvider theme={theme}>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+          `,
+        }}
+      />
       <Component {...pageProps} />
     </ChakraProvider>
   )
